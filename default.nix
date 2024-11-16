3876ed8858e51215ab5d8fa38de4c73a5b7091da
Evan Wang <evanwang0211@gmail.com>
2024-11-16 11:35:42 +0800

    add package: clevo-drivers

--- default.nix
+++ default.nix
@@ -12,9 +12,10 @@
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+#   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  clevo-drivers = pkgs.callPackage ./pkgs/os-specific/linux/clevo-drivers {};
+
 }
